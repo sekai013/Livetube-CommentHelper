f566e9a7acb8f28ad74ecf9f3f3e628ac8a06dcb
katsuya tanaka <sekai013@gmail.com>
2015-02-05 05:52:20 +0900

    implement 'copy word'

--- background/js/wordView.js
+++ background/js/wordView.js
@@ -5,7 +5,8 @@ App.WordView = Backbone.View.extend({
 	tagName: 'tr',
 
 	events: {
-		'click button#removeWord': 'onClickRemove'
+		'click button#removeWord': 'onClickRemove',
+		'click button#copyWord': 'onClickCopy'
 	},
 
 	render: function() {
@@ -23,5 +24,15 @@ App.WordView = Backbone.View.extend({
 		
 		this.model.destroy();
 		this.trigger('wordRemoved', content);
+	},
+
+	onClickCopy: function() {
+		var content  = this.$('#content').text();
+		var textArea = $('<textarea>').prop('value', content).css({ width: '1px' });
+
+		this.$el.append(textArea);
+		textArea.select();
+		document.execCommand('copy');
+		this.$(textArea).remove();
 	}
 });
