24366f04c3296d646684f148055e11fe031b9a58
katsuya tanaka <sekai013@gmail.com>
2015-02-06 07:48:56 +0900

    delete debug code

--- background/js/popup.js
+++ background/js/popup.js
@@ -4,31 +4,12 @@ window.App = {};
 
 $(function() {
 
-	var initializeAuthors = function() {
-		var authorCollection = new App.AuthorCollection([{
-			name : 'sekai',
-			id   : 'sekai',
-			words: ['this', 'is', 'sample']
-		}]);
-
-		authorCollection.each(function(author) {
-			author.save();
-		});
-
-		return authorCollection.models;
-	};
-
 	App.authorCollection = new App.AuthorCollection();
 	App.mainContainer    = new App.Container({
 		el: '#mainContainer'
 	});
 
 	App.authorCollection.fetch().then(function(authors) {
-		if(authors.length === 0) {
-			var models = initializeAuthors();
-			App.authorCollection.reset(models);
-		}
-
 		var urlRegExp = /http:\/\/[htz]?\.?livetube\.cc\/([^\/]*)\/[^\/]*/;
 		var query = {
 			active  : true,
